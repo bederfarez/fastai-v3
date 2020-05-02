4892b0ff6f9852a76e2a6a1ee660757687539fdf
bederfarez <63023693+bederfarez@users.noreply.github.com>
2020-05-01 21:00:44 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1anxH8VrkLtYuS-vx4VBNuWuZ3XAnwIde'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1anxH8VrkLtYuS-vx4VBNuWuZ3XAnwIde'
 export_file_name = 'export.pkl'
 
 classes = ['tower crane', 'crawler crane', 'crane truck']
